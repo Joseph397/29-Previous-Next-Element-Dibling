e6eb3be01f8ebf9eb2efdcc72f25c5c39330217d
name <gramkracker88@gmail.com>
2020-09-02 19:12:04 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// previousElementSibling
+// nextElementSibling
+// dosent count whtiespace
+
+const first = document.querySelector('.first');
+first.nextElementSibling.style.color = 'red';
+
+const last = document.querySelector('#last');
+last.previousElementSibling.style.color = 'blue';
